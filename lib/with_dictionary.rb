6eb4ebce511a220b38e4646f90c8567d1d029cce
Baylor Rae <baylor@thecodedeli.com>
2019-06-20 10:17:53 -0500

    find version closest to desired length

--- lib/with_dictionary.rb
+++ lib/with_dictionary.rb
@@ -5,6 +5,27 @@ class WithDictionary
   }
 
   def truncate(length, text)
-    text.split(' ').map { |word| DICTIONARY.fetch(word, word) }.join(' ')
+    words = text.split(' ')
+
+    known_words = words & DICTIONARY.keys
+    combinations = known_words.length.times
+      .flat_map { |x| known_words.combination(x + 1).to_a }
+
+    versions = []
+    combinations.each do |words_to_attempt|
+      versions << words.map do |word|
+        if words_to_attempt.include?(word)
+          DICTIONARY[word]
+        else
+          word
+        end
+      end.join(' ')
+    end
+
+    versions
+      .sort_by(&:length)
+      .reverse
+      .select { |version| version.length <= length }
+      .first
   end
 end
--- spec/with_dictionary_spec.rb
+++ spec/with_dictionary_spec.rb
@@ -2,7 +2,9 @@ require "rspec"
 require "./lib/with_dictionary"
 
 describe WithDictionary do
-  it "truncates by dictionary lookup" do
+  it "trucates closest to desired length" do
     expect(subject.truncate(14, 'small diagonal cross')).to eq('sm diag cross')
+    expect(subject.truncate(16, 'small diagonal cross')).to eq('small diag cross')
+    expect(subject.truncate(17, 'small diagonal cross')).to eq('sm diagonal cross')
   end
 end
